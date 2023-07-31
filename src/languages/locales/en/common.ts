3bf70447c7697c5607f607d4e14a71b4d08ab87f
nika <nikamunja31415@gmail.com>
2023-07-31 14:59:45 +0400

    add languages function

---
+++ src/languages/locales/en/common.ts
@@ -0,0 +1,12 @@
+export default {
+	chooseLanguage: "choose language",
+	joinUs: "Join our team",
+	next: "Next",
+	add: "Add",
+	agree: "Agree",
+	notifications: "notifications",
+	unread: "unread",
+	search: "search",
+	active: "active",
+	history: "history",
+};
